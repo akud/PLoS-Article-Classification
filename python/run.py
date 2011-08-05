16a4a4be95142bdf18334b764260406a83456ff6
Alex Kudlick <adkudlick@gmail.com>
2011-08-05 12:48:43 -0700

    changed data directory

--- python/run.py
+++ python/run.py
@@ -7,16 +7,16 @@ s = setup.sample(10000,6000)
 
 print datetime.now(), 'finished gathering sample articles'
 
-csv.writer(open('test_ids.csv','w')).writerow([f['id'] for f in s['test']])
-csv.writer(open('train_ids.csv','w')).writerow([f['id'] for f in s['train']])
+csv.writer(open('../data/test_ids.csv','w')).writerow([f['id'] for f in s['test']])
+csv.writer(open('../data/train_ids.csv','w')).writerow([f['id'] for f in s['train']])
 
-counter = words.counter([f[words.__text__][0] for f in s['train'] if len(f[words.__text__][0]) > 0)
-mapper = words.mapper([f[words.__subject__][0] for f in s['train'] if len(f[words.__subject__][0]) > 0)
+counter = words.counter([f[words.__text__][0] for f in s['train'] if len(f[words.__text__][0]) > 0],normalize=False,appendWordCount=True)
+mapper = words.mapper([f[words.__subject__][0] for f in s['train'] if len(f[words.__subject__][0]) > 0])
 
-train = csv.writer(open('train.csv','w'))
-test = csv.writer(open('test.csv','w')) 
-ytrain = csv.writer(open('ytrain.csv','w')) 
-ytest = csv.writer(open('ytest.csv','w')) 
+train = csv.writer(open('../data/train.csv','w'))
+test = csv.writer(open('../data/test.csv','w')) 
+ytrain = csv.writer(open('../data/ytrain.csv','w')) 
+ytest = csv.writer(open('../data/ytest.csv','w')) 
 
 print datetime.now(), 'converting to vectors and storing to csv'
 for f in [f[words.__text__][0] for f in s['train'] if len(f[words.__text__][0]) > 0]:
