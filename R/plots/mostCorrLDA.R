92cd537066c4d383315019f7e0867b224a2ac31c
Alex Kudlick <adkudlick@gmail.com>
2011-08-10 22:16:24 -0700

    fixed graph title, etc.

--- R/plots/mostCorrLDA.R
+++ R/plots/mostCorrLDA.R
@@ -1,17 +1,11 @@
 source('setup.R')
 
-x <- seq(20,1000,by=20)
+x <- 10:20
 
 plainErrors <- vector()
 pcErrors <- vector()
 
 for (i in x) {
-    common.log('running LDA with',i,'most correlated PCs and columns')
-    plain <- function(X,Y) lda.mostCorrelated(X,Y,i)
-    error <- cross.validation(trainingData,yTrain,5,plain)
-    plainErrors <- c(plainErrors,error)
-    common.log('Error for most correlated columns:',error)
-    rm(plain)
     pc <- function(X,Y) lda.pcMostCorrelated(X,Y,i)
     error <- cross.validation(trainingData,yTrain,5,pc)
     pcErrors <- c(pcErrors,error)
@@ -19,9 +13,7 @@ for (i in x) {
     rm(pc)
 }
 
-jpeg('plots/lda_images/lda_mostCorr.jpg')
-plot(x,plainErrors,type='l',col='orange',xlab='k',ylab='Expected Error',
-    main='10-fold Cross Validation Error')
-lines(x,pcErrors,col='turquoise')
-legend('topleft',c('Most Correlated PCs','Most Correlated Columns'),fill=c('turquoise','orange'))
+jpeg('plots/performance_images/lda_mostCorr10_20.jpg')
+plot(x,pcErrors,type='l',col='orange',xlab='k',ylab='Expected Error',
+    main='5-fold Cross Validation Error')
 dev.off()
