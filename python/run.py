af8b2f7d5c42649834c967e424fb641247de4021
Alex Kudlick <adkudlick@gmail.com>
2011-10-25 11:35:54 -0700

    added support for tf-idf vectors

--- python/run.py
+++ python/run.py
@@ -2,8 +2,8 @@
 
 import setup, csv, words
 from datetime import datetime
-subjectField = 'cross_published_journal_key'
-textField = 'abstract'
+subjectField = 'subject2'
+textField = 'title'
 
 s = setup.sample(15000,10000,subjectField,textField)
 
@@ -35,12 +35,12 @@ ytest = csv.writer(open('../data/ytest.csv','w'))
 
 print datetime.now(), 'converting to vectors and storing to csv'
 for f in [f[textField] for f in s['train'] if 1 in mapper.vector(f[subjectField][0])]:
-    train.writerow(counter.vector(f))
+    train.writerow(counter.tf_vector(f))
 for f in [f[subjectField][0] for f in s['train'] if 1 in mapper.vector(f[subjectField][0])]:
     ytrain.writerow(mapper.vector(f))
 
 for f in [f[textField] for f in s['test']]:
-    test.writerow(counter.vector(f))
+    test.writerow(counter.tf_vector(f))
 for f in [f[subjectField][0] for f in s['test']]:
     ytest.writerow(mapper.vector(f))
 
--- python/words.py
+++ python/words.py
@@ -1,73 +1,100 @@
-import plos, string, re, csv
+import plos, re, csv
 from Stemmer import Stemmer
+from math import log
+
+__stopwords__ = [f.replace('\n','') for f in open('stopwords.txt').readlines()]
+__re__ = re.compile('[,0-9]')
+__stemmer__ = Stemmer('english')
+
+def clean(word):
+    word = __re__.sub('',word.lower())
+    return __stemmer__.stemWord(word)
+
+def accept(word):
+    word = __re__.sub('',word.lower())
+    return len(word) > 0 and word not in __stopwords__ 
 
 class counter:
     '''
-        Object to build word count vectors from texts.  Instances are created with 
-        a whole set of training documents from which to build a global dictionary.
+    Object to build word-count vectors using a corpus.  Must be constructed using a corpus of texts so that a 
+    global dictionary can be created; thereafter vectors are created only from the words in the dictionary.
+
+    In addition to the vector methods, this provides access to three properties that may be useful:
+    doccounts - a map of words and the number of documents in which they appear
+    words - a sorted list of the words
+    total_docs - the total number of documents used in the dictionary
     '''
 
-    def __init__(self,texts,mindocs=1,maxdocs=None,normalize=True,appendWordCount=False,dictionaryFile=None):
+    def __init__(self,texts,mindocs=1,maxdocs=None,dictionaryFile=None):
         '''
         texts - training set of texts from which to build a global dictionary
         mindocs - the minimum number of documents a (stemmed) word must appear in to be included in the dictionary
         maxdocs - the maximum number of documents a (stemmed) word can appear in to be included in the dictionary
-        normalize - whether vectors produced by this object should have their entries divided by the total number of words
-        appendWordCount - whether to append the word count to the end of vectors (after normalization, if any)
         dictionaryFile - name of a file in which to store the dictionary, if any
         '''
 
-        self.stopwords = [f.replace('\n','') for f in open('stopwords.txt').readlines()]
-        self.stemmer = Stemmer('english')
-        self.normalize = normalize
-        self.appendWordCount = appendWordCount
-        self.re = re.compile('[^a-zA-Z]')
-#build the global dictionary
+        self.doccounts = {}
+        self.words = []
+        self.total_docs = 0
+
         if isinstance(texts,str):
             texts = [ texts ]
         elif isinstance(texts[0],list):
             texts = [t[0] for t in texts]
         words = [ f.split() for f in texts ]
-        words = [ [self.clean(f) for f in lst if f.lower() not in self.stopwords] for lst in words ]
-#count the number of times each word appears in documents
-        counts = {}
+        words = [ [clean(f) for f in lst if accept(f)] for lst in words ]
+        if not maxdocs: maxdocs = len(texts) 
+        self.total_docs = len(texts)
+    
+        #count the number of times each word appears in documents
         for lst in words:
             for word in set(lst):
-                if counts.has_key(word):
-                    counts[word] = counts[word] + 1
+                if self.doccounts.has_key(word):
+                    self.doccounts[word] = self.doccounts[word] + 1
                 else:
-                    counts[word] = 1
-        if not maxdocs: maxdocs = len(texts) 
-        self.dic = sorted([word for word in counts.keys() if counts[word] >= mindocs and 
-            counts[word] <= maxdocs and len(word) > 0])
-        print 'created dictionary of %s stemmed words, excluded %s words' % (len(self.dic),len(counts) - len(self.dic))
+                    self.doccounts[word] = 1
+        total_words = len(self.doccounts)
+        #check for words that appear too frequently or infrequently, and remove them
+        self.doccounts = dict((word,count) for word, count in self.doccounts.iteritems() if count >= mindocs and count <= maxdocs)
+        #store sorted words so vectors are always in the same order
+        self.words = sorted(self.doccounts.keys()) 
+        #cache the idf values
+        self.__idfcache__ = [ log(float(self.total_docs) / self.doccounts[word]) for word in self.words ] 
+
+        print 'created dictionary of %s stemmed words, excluded %s words' % (len(self.doccounts),total_words - len(self.doccounts))
+
         if dictionaryFile:
             dictionaryFile = open(dictionaryFile,'w')
-            dictionaryFile.writelines([f + '\n' for f in self.dic])
+            dictionaryFile.writelines([f + '\n' for f in self.words])
             dictionaryFile.flush()
             dictionaryFile.close()
 
-    def vector(self,text):
+    def count_vector(self, text):
         '''
-        Build a vector of word counts from the global dictionary in the given text
+        Create a vector of word counts - for each word in the dictionary, count the occurrences in the given text
         '''
         if(isinstance(text,list)):
             text = text[0]
         words = text.split()
-        words = [self.clean(f) for f in words if f.lower() not in self.stopwords]
-        counts = [ words.count(f) for f in self.dic]
-        if self.normalize: 
-            total = reduce(lambda x,y : x + y, counts)
-            try:
-                counts = [ float(i) / total for i in counts ]
-            except ZeroDivisionError:
-                counts = [0 for i in counts ]
-        if self.appendWordCount: counts.append(len(words))
-        return counts
+        words = [ clean(f) for f in words if accept(f) ]
+        return [ words.count(f) for f in self.words ]
+ 
+    def tf_vector(self, text):
+        '''
+        Create a vector of term frequency values - for each word in the dictionary, return the frequency with which it
+        occurs in the given text (the word count divided by the number of dictionary words in the text)
+        '''
+        counts = self.count_vector(text)
+        total = reduce(lambda x,y : x + y, counts)
+        return [ float(i) / total for i in counts ]
 
-    def clean(self,word):
-        word = self.re.sub('',word.lower()) 
-        return self.stemmer.stemWord(word)
+    def tfidf_vector(self, text):
+        '''
+        Create a vector of term frequency-inverse document frequency values - for each word in the dictionary, 
+        return the TF-IDF value: http://en.wikipedia.org/wiki/Tfidf 
+        '''
+        tf_vector = self.tf_vector(text)
+        return [ tf*idf for tf, idf in zip(tf_vector,self.__idfcache__) ]
 
 class mapper:
     '''
