f7a498c5316616a937f77c105a7f705d3f8ef085
Alex Kudlick <adkudlick@gmail.com>
2011-10-31 11:40:35 -0700

    changed to using subject hierarchy

--- scripts/gather_sample.py
+++ scripts/gather_sample.py
@@ -3,10 +3,10 @@ import plos_classification.setup as setup, json
 from datetime import datetime
 
 print datetime.now(), 'gathering sample articles'
-s = setup.sample(15000,10000,'abstract','title','subject2','cross_published_journal_key')
+s = setup.sample(15000,10000,'abstract','title','subject2_hierarchy','cross_published_journal_key')
 
 print datetime.now(), 'writing sample to file'
-o = open('../data/sample.json','w')
+o = open('data/sample.json','w')
 json.dump(s,o,indent=2)
 
 print datetime.now(),'finished'
