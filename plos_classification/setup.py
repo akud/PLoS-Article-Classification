85b531ff3ea600bd1d8bacd5e6f22081e4360343
Alex Kudlick <adkudlick@gmail.com>
2011-10-31 10:16:04 -0700

    added unit test for sample gathering module

--- plos_classification/setup.py
+++ plos_classification/setup.py
@@ -2,23 +2,25 @@ import solr, words
 from urllib2 import quote
 import csv
 
-__incSize__ = 500
+__defaultInc__ = 500
 
 def sample(limit,trainingSetSize,*args):
     def accept(doc):
         haskeys = False not in [doc.has_key(field) for field in args] 
         if haskeys:
             return 0 not in [len(doc[field][0])
-            if isinstance(doc[field],list) else len(doc[field]) for field in args]
+                if isinstance(doc[field],list) else len(doc[field]) for field in args]
         else:
             return False
     fields = 'id,%s' % (','.join(args))
     sort = quote('publication_date asc') 
     documents = []
-    for f in range(0,limit,__incSize__):
+    increment = __defaultInc__ if limit > __defaultInc__ else limit
+    for f in range(0,limit,increment):
+        if f + increment > limit: increment = limit - increment
         documents += solr.search(
             {'fl' : fields, 'sort' : sort,
-            'rows' : __incSize__, 'start' : f})  
+            'rows' : increment, 'start' : f})  
 
     documents = filter(accept, documents)
     return { 'train' : documents[0:trainingSetSize],
---
+++ plos_classification/test/test_setup.py
@@ -0,0 +1,21 @@
+from unittest import TestCase, main
+import plos_classification.setup as setup
+
+class TestLimit(TestCase):
+
+    def runTest(self):
+        sample = setup.sample(100,85,'id','abstract','title')
+        self.assertTrue(len(sample['train']) <= 85)
+        self.assertTrue(len(sample['test']) <= 15)
+
+        for f in sample['train']:
+            for arg in ['id','abstract','title']:
+                self.assertIn(arg,f.keys())
+
+        for f in sample['test']:
+            for arg in ['id','abstract','title']:
+                self.assertIn(arg,f.keys())
+    
+
+if __name__ == '__main__':
+    unittest.main()
