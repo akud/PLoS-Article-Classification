2347c1b224651bfcceb8f657da4c920471620edd
Alex Kudlick <adkudlick@gmail.com>
2011-11-03 11:53:49 -0700

    added support for loading numpy arrays

--- plos_classification/load_data.py
+++ plos_classification/load_data.py
@@ -1,13 +1,31 @@
-import csv
+'''
+Holds functions for loading data from csv files
+'''
+import csv, numpy
 
-def load(xfile,yfile):
+def load(xfile,yfile,np=False):
+    '''
+    Load the data from the specified csv files
+    xfile - file-like object or string pointing to csv holding x data
+    yfile - file-like object or string pointing to csv holding y data
+    np - if true, return the data as numpy arrays
+    '''
     x = []
     y = []
-    for row in csv.reader(open(xfile)):
+    if isinstance(xfile,str) or isinstance(xfile,unicode):
+        xfile = open(xfile)
+    if isinstance(yfile,str) or isinstance(yfile,unicode):
+        yfile = open(yfile)
+
+    for row in csv.reader(xfile):
         x.append([float(f) for f in row])
-    for row in csv.reader(open(yfile)):
+
+    for row in csv.reader(yfile):
         try:
             y.append(row.index('1'))
         except ValueError:
             y.append(-1)
-    return x,y
+    if not np:
+        return x,y
+    else:
+        return numpy.asarray(x),numpy.asarray(y)
--- plos_classification/test/test_load_data.py
+++ plos_classification/test/test_load_data.py
@@ -1,5 +1,6 @@
 from unittest import main, TestCase
 import plos_classification.load_data as load_data
+import numpy, json
 
 class TestLoadData(TestCase):
     def setUp(self):
@@ -14,10 +15,27 @@ class TestLoadData(TestCase):
             1,
             -1
         ]
-    def runTest(self):
+    def test_standard_load(self):
         x,y = load_data.load(self.xFile,self.yFile) 
         self.assertEqual(x,self.expectedX)
         self.assertEqual(y,self.expectedY)
-    
+
+    def test_numpy_load(self):
+        x,y = load_data.load(self.xFile,self.yFile,np=True) 
+        array_type = type(numpy.array([]))
+        self.assertIsInstance(x,array_type)
+        self.assertIsInstance(y,array_type)
+
+        self.assertEqual(len(x),len(self.expectedX))
+        for i in range(0,len(x)):
+            self.assertEqual(len(x[i]),len(self.expectedX[i]))
+            for j in range(0,len(x[i])):
+                self.assertEqual(x[i,j],self.expectedX[i][j])
+
+        self.assertEqual(len(y),len(self.expectedY))
+        for i in range(0,len(y)):
+            self.assertEqual(y[i],self.expectedY[i])
+
+
 if __name__ == '__main__':
     main()
